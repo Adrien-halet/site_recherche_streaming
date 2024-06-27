0b17b691282ff89bec2960559a30c45bd5bf4f31
Adrien-halet <148947186+Adrien-halet@users.noreply.github.com>
2024-06-27 16:26:44 +0200

    authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,38 +1,32 @@
-<?php
-session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "utilisateur";
-
-// Connexion à la base de données
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$user = $_POST['username'];
-$pass = $_POST['password'];
-
-// Préparation et exécution de la requête
-$sql = $conn->prepare("SELECT * FROM utilisateurs WHERE username = ?");
-$sql->bind_param("s", $user);
-$sql->execute();
-$result = $sql->get_result();
-
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    // Vérification du mot de passe
-    if (password_verify($pass, $row['password'])) {
-        $_SESSION['username'] = $user;
-        header("Location: dashboard.php");
-        exit();
-    } else {
-        echo "Mot de passe incorrect.";
-    }
-} else {
-    echo "Nom d'utilisateur incorrect.";
-}
-
-$conn->close();
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/authenticate.css">
+    <title>Authentification</title>
+</head>
+<body>
+    <div class="auth-container">
+        <div class="nom_site">**********</div>
+        <div class="auth-buttons">
+            <button class="auth-button">Connectez-vous</button>
+            <div class="separator">
+                <span>ou</span>
+            </div>
+            <button class="auth-button">
+                <a href="inscription.php">
+                    Inscrivez-vous
+                </a>
+            </button>
+        </div>
+        <div class="forgot-password">
+            <a href="#">Mot de passe oublié ?</a>
+        </div>
+        <div class="remember-me">
+            <input type="checkbox" id="remember-me">
+            <label for="remember-me">Se souvenir de moi</label>
+        </div>
+    </div>
+</body>
+</html>
